bf79a1fe42d1a693611d4b747614e447e54c1527
Ryoichi <shiohama@hamalt.studio>
2022-04-22 12:04:04 +0900

    Tailwind CSSが動作する状態でコミット

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    `./components/**/*.{vue,js}`,
+    `./layouts/**/*.vue`,
+    `./pages/**/*.vue`,
+    `./composables/**/*.{js,ts}`,
+    `./plugins/**/*.{js,ts}`,
+    `./App.{js,ts,vue}`,
+    `./app.{js,ts,vue}`,
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
